0369d42f58c4bd4bcf14d1565667aac950f6685a
Aditeya <aditeya.baral@gmail.com>
2022-07-19 22:06:23 +0530

    Update setup.py with supported Python versions

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
